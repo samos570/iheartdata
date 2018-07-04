1f98ddfc7edbe72e3b81863a095366b0c15bdb24
Justin Keller <jck26@psu.edu>
2018-07-04 13:32:59 -0400

    Add CVC nonsense words

---
+++ CVCnonsense/buildsheet.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Create the worksheet.tex file
+cd Python
+python randomCVCn.py
+
+# Compile all .tex files to pdf
+cd ../Worksheet
+pdflatex worksheet.tex
+cd ../Coversheet
+pdflatex coversheet.tex
+cd ../../AccuracyTracking
+pdflatex AccuracyTracking.tex
+cd ../AccuracyPlotting
+pdflatex AccuracyPlotting.tex
+cd ../CVCnonsense
+
+# Merge pdfs
+pdftk ./Coversheet/coversheet.pdf ../AccuracyTracking/AccuracyTracking.pdf ../AccuracyPlotting/AccuracyPlotting.pdf ./Worksheet/worksheet.pdf cat output CVCnonsense.pdf
+
+
